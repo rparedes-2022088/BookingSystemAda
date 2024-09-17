71b7e2ccc9902043be0f03f8e39f88e8cb63774c
rparedes-2022088 <rparedes-2022088@kinal.edu.gt>
2024-09-17 17:23:26 -0600

    Inicio proyecto Booking system

---
+++ src/test/java/org/adaschool/BookingSystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.BookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
